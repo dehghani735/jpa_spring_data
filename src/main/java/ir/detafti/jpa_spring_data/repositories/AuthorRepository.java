b5f6741961b5640f280feea27fc5bb113bc287ac
mr.detafti <dehghani735@gmail.com>
2024-02-10 14:20:54 +0330

    polished.

--- src/main/java/ir/detafti/jpa_spring_data/repositories/AuthorRepository.java
+++ src/main/java/ir/detafti/jpa_spring_data/repositories/AuthorRepository.java
@@ -1,21 +1,22 @@
 package ir.detafti.jpa_spring_data.repositories;
 
 import ir.detafti.jpa_spring_data.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AuthorRepository extends Repository<Author, Integer> {
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
     @Query("""
             SELECT a FROM Author a WHERE a.id = :id
             """)
     Optional<Author> findAuthorById(Integer id); // Select a from Author a where a.id = :id
 
-    @Query("""
-            SELECT a FROM Author a
-             """)
-    List<Author> findAllAuthors();
+//    @Query("""
+//            SELECT a FROM Author a
+//             """)
+//    List<Author> findAllAuthors();
 }
